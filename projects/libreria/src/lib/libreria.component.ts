57b08f133e8df07c43e0e385f2dcc3d94ce35d8e
Agustina <agustina.perezserra@gmail.com.ar>
2022-06-02 08:39:54 -0300

    first commit

---
+++ projects/libreria/src/lib/libreria.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-libreria',
+  template: `
+    <p>
+      libreria works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class LibreriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
